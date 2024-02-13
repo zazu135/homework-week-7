bbbea5565a7f66564e6abc4aa411dc260134cf3a
zazu135 <142782991+zazu135@users.noreply.github.com>
2024-02-13 15:38:20 +0400

    added functions and fixed typos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+function refreshWeather (response) {
+    let temperatureElement =document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+  let apiKey = "1feb8a7b2d092f38fof7t1c4fa0416b7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 
 }
 
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit)
+
+searchCity("Toronto");
